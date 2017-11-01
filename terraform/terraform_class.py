321072d3eceecc8042e3067d9361809472c1e0a2
Rory Chatterton <rorychatt@gmail.com>
2017-11-01 22:20:42 +1100

    Added function to run and apply runs, returning exit codes. Next step is to integrate with JenkinsFile

--- terraform/terraform_class.py
+++ terraform/terraform_class.py
@@ -1,14 +1,15 @@
-import requests
 import json
 import os
-import hcl
-import consul
 
+import consul
+import hcl
+import requests
+import time
 
 class TerraformAPICalls():
     base_url = "https://atlas.hashicorp.com/api/v2"
 
-    def __init__(self, organization, directory, atlas_token=None, base_api_url=None):
+    def __init__(self, organization, directory, app_id, atlas_token=None, base_api_url=None):
 
         # Get Token from Environment Variable if not passed.
         atlas_token = os.environ["ATLAS_TOKEN"]
@@ -17,18 +18,9 @@ class TerraformAPICalls():
             'Content-Type': 'application/vnd.api+json'
         }
 
+        self.app_id = app_id
         self.organization = organization
         self.directory = directory
-
-        # Read Metafile
-        with open(self.directory + "/meta.tfvars", 'r') as fp:
-            variable_list = hcl.load(fp)
-
-            self.app_id = variable_list["app_id"]
-            self.app_name = variable_list["app_name"]
-            self.cost_centre = variable_list["cost_centre"]
-            self.git_repository = variable_list["git_repository"]
-            self.environments = variable_list["environments"]
 
     def get_workspaces(self):
         workspaces_url = self.base_url + "/organizations/" + self.organization + "/workspaces"
@@ -66,7 +58,7 @@ class TerraformAPICalls():
                     "linkable-repo-id": repo_id,
                     "ingress-trigger-attributes": {
                         "default-branch": "true",  # NOT CONFIGURABLE IN BETA. TO BE REPLACED WITH DEFAULT BRANCH
-                        "branch": branch,          # NOT CONFIGURABLE IN BETA. TO BE REPLACED WITH DEFAULT BRANCH
+                        "branch": branch,  # NOT CONFIGURABLE IN BETA. TO BE REPLACED WITH DEFAULT BRANCH
                         "vcs_root_path": "",
                         "ingress-submodules": "false",
                     }
@@ -149,17 +141,17 @@ class TerraformAPICalls():
     def generate_workspace_name(self, environment):
         return (self.app_id + "-" + environment + "-" + self.app_name).replace(" ", "_")
 
-    def generate_workspaces(self):
+    def generate_workspaces(self, app_id, app_name, cost_centre, git_repository):
         c = consul.Consul(host=os.environ["CONSUL_ADDRESS"])
 
         # Write MetaFile to Consul
         base_keypath = "apps/" + self.app_id + "/"
 
         # Write MetaFile to Consul
-        c.kv.put(base_keypath + "app_id", self.app_id)
-        c.kv.put(base_keypath + "app_name", self.app_name)
-        c.kv.put(base_keypath + "cost_centre", self.cost_centre)
-        c.kv.put(base_keypath + "git_repository", self.git_repository)
+        c.kv.put(base_keypath + "app_id", app_id)
+        c.kv.put(base_keypath + "app_name", app_name)
+        c.kv.put(base_keypath + "cost_centre", cost_centre)
+        c.kv.put(base_keypath + "git_repository", git_repository)
 
         # Generate Workspaces from Environment Section of Meta
         for environment in self.environments:
@@ -171,8 +163,8 @@ class TerraformAPICalls():
 
     def get_run_status(self, run_id="run-jDDckmxjRA1rTXVx"):
         request_uri = self.base_url + "/runs/" + run_id
-        data =  json.loads(requests.get(request_uri, headers=self.header).text)
-        return(data['data']['attributes']['status'])
+        data = json.loads(requests.get(request_uri, headers=self.header).text)
+        return data['data']['attributes']['status']
 
     def discard_untriggered_plans(self, workspace_id):
         request_uri = self.base_url + "/workspaces/" + workspace_id + "/runs"
@@ -188,17 +180,17 @@ class TerraformAPICalls():
             for obj in data['data']:
 
                 # Delete Item
-                if (obj["attributes"]["status"] == "planned"):
+                if obj["attributes"]["status"] == "planned":
                     print("Discarding: " + obj["id"])
                     self.discard_plan(obj["id"])
 
                 # More items left to Delete
-                elif (obj["attributes"]["status"] == "pending" or obj["attributes"]["status"] == "planning"):
+                elif obj["attributes"]["status"] == "pending" or obj["attributes"]["status"] == "planning":
                     nothing_to_discard = False
 
     def discard_plan(self, run_id):
         request_uri = self.base_url + "/runs/" + run_id + "/actions/discard"
-        data = {"comment":"Dropped by Jenkins Build"}
+        data = {"comment": "Dropped by Jenkins Build"}
 
         requests.post(request_uri, headers=self.header, data=json.dumps(data)).text
 
@@ -210,24 +202,23 @@ class TerraformAPICalls():
         request_uri = self.base_url + "/runs"
 
         data = {
-          "data": {
-            "attributes": {
-              "is-destroy":destroy
-            },
-            "type":"runs",
-            "relationships": {
-              "workspace": {
-                "data": {
-                  "type": "workspaces",
-                  "id": workspace_id
+            "data": {
+                "attributes": {
+                    "is-destroy": destroy
+                },
+                "type": "runs",
+                "relationships": {
+                    "workspace": {
+                        "data": {
+                            "type": "workspaces",
+                            "id": workspace_id
+                        }
+                    }
                 }
-              }
             }
-          }
         }
 
-
-        return_data =  requests.post(request_uri, headers=self.header, data=json.dumps(data))
+        return_data = requests.post(request_uri, headers=self.header, data=json.dumps(data))
 
         print("Creating new Terraform run against: " + workspace_id)
 
@@ -235,57 +226,105 @@ class TerraformAPICalls():
         if str(return_data.status_code).startswith("2"):
             print("New Run: " + json.loads(return_data.text)['data']['id'])
 
-            # Check to see if changes have been made
-            print("Server Output: " + json.loads(return_data.text)["data"]["attributes"]["message"])
+            # Keep Checking until planning phase has finished
+            planning = True
+            status = "planning"
+            changes_detected = False
+            while planning:
+                planning = False
+                print("Job Status: Planning")
+                time.sleep(5)
 
-            print(
-                json.loads( requests.get(
-                        self.base_url + "/runs/" + json.loads(return_data.text)['data']['id'],
-                        headers=self.header
-                    )).text['data']['attributes']['status']
-            )
+                request = json.loads(requests.get(
+                    self.base_url + "/runs/" + json.loads(return_data.text)['data']['id'],
+                    headers=self.header
+                ).text)
 
-            with open('data.txt', 'w') as f:
-                json.dump({ "run_id": json.loads(return_data.text)['data']['id'] }, f, ensure_ascii=False)
+                status = request['data']['attributes']['status']
+                changes_detected = request['data']['attributes']['has-changes']
+                if status == "planning":
+                    planning = True
+
+            # If Plan Failed
+            if status == "errored":
+                print("Job Status: Failed")
+                print("Job Output")
+                exit(1)
+
+            # If Plan Succeeded, Check for Changes
+            elif status == "planned":
+                if not changes_detected:
+                    print("No Changes Detected")
+                    exit(0)  # Exit if no changes
+
+                print("Changes Detected")
+                with open('data.txt', 'w') as f:
+                    json.dump({"run_id": json.loads(return_data.text)['data']['id']}, f, ensure_ascii=False)
+                exit(2)
 
             exit(0)
 
-        else: # Else Fail Run
-            print("Request Reply: " + json.loads(return_data.text)["data"]["attributes"]["message"])
+        else:  # Else Fail Run
+            print("Plan Failed: " + json.loads(return_data.text)["data"]["attributes"]["message"])
             exit(1)
 
     def apply_run(self, workplace_id, run_id, destroy=False):
         request_uri = self.base_url + "/runs/" + run_id + "/actions/apply"
 
         data = {
-          "data": {
-            "attributes": {
-              "is-destroy":destroy
-            },
-            "relationships": {
-              "workspace": {
-                "data": {
-                  "type":"workspaces",
-                  "id": workplace_id
-                }
-              }
-            },
-            "type":"runs"
-          }
+            "data": {
+                "attributes": {
+                    "is-destroy": destroy
+                },
+                "relationships": {
+                    "workspace": {
+                        "data": {
+                            "type": "workspaces",
+                            "id": workplace_id
+                        }
+                    }
+                },
+                "type": "runs"
+            }
         }
 
-        return_data =  requests.post(request_uri, headers=self.header, data=json.dumps(data)).text
-        return (return_data['data']['id'])
+        print("Applying Job: " + run_id)
+        return_data = requests.post(request_uri, headers=self.header, data=json.dumps(data))
 
+        if str(return_data.status_code).startswith("2"):
 
+            # Keep Checking until planning phase has finished
+            status = "applying"
+            while status == "applying" or status == "queued":
+                print("Job Status: applying changes")
+                time.sleep(5)
 
-workplace = TerraformAPICalls(organization="westpac-v2", directory='C:\Repositories\Orchestration\Apps\A00001-Bamboo')
+                request = json.loads(requests.get(self.base_url + "/runs/" + run_id, headers=self.header).text)
+
+                status = request['data']['attributes']['status']
+
+            # If Plan Failed
+            if status == "errored":
+                print("Job Status: Failed")
+                print("Job Output TBA")
+                exit(1)
+
+            # If Plan Succeeded, Check for Changes
+            elif status == "applied":
+                print("Job Status: Applied Successfully!")
+                exit(0)
+
+        else:  # Else Fail Run
+            print("Apply Failed")
+            exit(1)
+
+workplace = TerraformAPICalls(organization="westpac-v2", directory='C:\Repositories\Orchestration\Apps\A00001-Bamboo', app_id="XXX001")
 workspace_id = workplace.get_workspace_id("XXX001-odev-My_Test_Application")
-run_id = workplace.create_run(workspace_id)
-# apply = workplace.apply_run(workspace_id,run_id)
-# print(apply)
+#run_id = workplace.create_run(workspace_id)
+apply = workplace.apply_run(workspace_id, "run-Z3obZbjZW29xERCz")
 
-#workplace.discard_untriggered_plans(workspace_id)
+
+# workplace.discard_untriggered_plans(workspace_id)
 
 # API_Calls.LoadLocalAzureCredentials("core-odev")
 # API_Calls.LoadVariables("test.tfvars", "core-odev")
